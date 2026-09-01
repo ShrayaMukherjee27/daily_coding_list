b9584644f7db0471e49ccae6473917196db4a461
Shraya <mukherjeeshraya72@gmail.com>
2026-09-01 23:29:13 +0530

    Create houseRobber.cpp

---
+++ DynamicProgramming/houseRobber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        vector<int> dp(3);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[2] = max(nums[i]+dp[0] , dp[1]);
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+        return dp[2];
+    }
+};
